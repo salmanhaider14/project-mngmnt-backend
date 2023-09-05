5d3a82e1a8153dd6f84c71d5b120e1102139f633
unknown <salmanpatrick5@gmail.com>
2023-09-06 00:27:52 +0500

    let's see

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 import checkUserRole from "../middlewares/roleCheck.js";
 import jwt from "jsonwebtoken";
